849b347bab0c655c2b52769b1df0e72ed295a662
Исаков Михаил Андреевич <maisakov@edu.hse.ru>
2024-04-29 17:38:25 +0300

    update keyboards

--- handlers/user/survey/utils.py
+++ handlers/user/survey/utils.py
@@ -100,7 +100,7 @@ async def send_next_question(bot: Bot, user_id_tel: uint32,
                 schema.append(len(list(group)))
 
             keyboard = ic._create_kb(actions=action_list, schema=schema)
-            return await bot.send_message(user_id_tel, text=question_template,
+            return await bot.send_message(user_id_tel, text=question_template + '                             ',
                                 reply_markup=keyboard)
         else:
             return await bot.send_message(user_id_tel, text="Cвободных дат нет!\nНапишите /finish, чтобы преждевременно завершить запись!")
